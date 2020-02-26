1109886e15ef7bf4021acb0c6cbb7f6489d30cd1
Jeffrey <jeffrey.liu90@gmail.com>
2020-02-26 22:05:33 +0000

    implemented the following functions: have an alert pop up if you have too many words or the text field is empty, and fetching tweet data and showing it on the screen

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,27 +1,117 @@
-/*
+/* moment.js
+unix time stamp
  * Client-side JS logic goes here
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-$(document).ready(function() {
+$(document).ready(function () {
   // Handler for .ready() called.
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-    "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-    "created_at": 1461116232227
+
+  //   const $form = $(".form-inline");
+  //   const $textarea = $("textarea").val();
+  // $form.on("submit", event => {
+  //   event.preventDefault()
+  //   if($textarea === "") {
+  //     alert("invalidDDD")
+  //   } 
+  //   $.ajax ({
+  //   url: '/tweets',
+  //   type: 'POST',
+  //   data: $form.serialize()
+
+  //   })
+  // })
+
+
+
+
+  //   const $form = $(".form-inline");
+  //   const $textarea = $("textarea").val();
+  // $form.on("submit", event => {
+  //   event.preventDefault()
+  //   if(isEntryValid($textarea)) {
+  //     alert("invalidDDD")
+  //   } else {
+  //   $.ajax ({
+  //   url: '/tweets',
+  //   type: 'POST',
+  //   data: $form.serialize()
+
+  //   })
+  // }})
+
+  const $form = $(".form-inline");
+
+  $form.on("submit", event => {
+    event.preventDefault()
+
+
+    const textarea = $(".new-tweet textarea").val();
+    if (isEntryValid(textarea) === false) {
+      alert("invalid entry")
+    } else if (entryLength(textarea) === false) {
+      alert("youre input is too long")
+    } else {
+      $.ajax({
+        url: '/tweets',
+        type: 'POST',
+        data: $form.serialize()
+
+      })
+    }
+  })
+
+
+  const entryLength = (entry) => {
+    if (entry.length > 140) {
+      return false
+    } else {
+      return true
+    }
   }
 
-let createTweetElement = function(tweeter) {
-  const {name, avatars, handle} = tweeter.user;
-  const content = tweeter.content.text;
-  const created = tweeter.created_at;
-  return  `<article class="tweets">
+  const isEntryValid = (entry) => {
+
+    if (entry === "") {
+      return false
+    } else {
+
+    return true
+  };
+  }
+
+  const loadTweets = function () {
+    $.ajax({
+      url: "/tweets",
+      type: "GET",
+      data: "JSON"
+    })
+      .then(data => {
+        renderTweets(data)
+
+      })
+  }
+
+  loadTweets()
+
+
+  // const renderTweets = function (tweets) {
+  //   for (let info of tweets) {
+  //     $('#tweets-container').append(createTweetElement(info));
+  //   }
+  // }
+
+
+
+
+  let createTweetElement = function (tweeter) {
+    const { name, avatars, handle } = tweeter.user;
+    const content = tweeter.content.text;
+    const created = tweeter.created_at;
+    // const days = Math.floor(created / (24 * 3600)) 
+
+
+    return `<article class="tweets">
   <header class="tweetHeader1">
     <div class="articleHead">
       <img class="minilogo" src=${avatars} alt="">
@@ -46,91 +136,24 @@ let createTweetElement = function(tweeter) {
     </div>
   </footer>
 </article>`
-}
+  }
 
 
-// Test / driver code (temporary). Eventually will get this from the server.
+  // Test / driver code (temporary). Eventually will get this from the server.
+
+  const renderTweets = function (tweets) {
+    for (let info of tweets) {
+      $('#tweets-container').append(createTweetElement(info));
+    }
+  }
 
 
-const $tweet = createTweetElement(tweetData);
 
-// Test / driver code (temporary)
-// console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+
+
+
 
 
 });
 
-$(document).ready(function() {
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
 
-
-const renderTweets = function (tweets) {
-  for (let info of tweets) {
-    $('#tweets-container').append(createTweetElement(info));
-  }
-}
-// For each? or for of
-
-
-
-
-
-let createTweetElement = function (tweeter) {
-  const { name, avatars, handle } = tweeter.user;
-  const content = tweeter.content.text;
-  const created = tweeter.created_at;
-
-  return `<article class="tweets">
-  <header class="tweetHeader1">
-    <div class="articleHead">
-      <img class="minilogo" src=${avatars} alt="">
-      <p> ${name}</p>
-    </div>
-    <div class="tweeter-handle">
-      <p>${handle}</p>
-    </div>
-  </header>
-  <body>
-    <p>${content}</p>
-  </body>
-  <hr>
-  <footer class="footer">
-    <p>${created}</p>
-    <div>
-      <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-      </ul>
-    </div>
-  </footer>
-</article>`
-}
-
-console.log(renderTweets(data))
-})
